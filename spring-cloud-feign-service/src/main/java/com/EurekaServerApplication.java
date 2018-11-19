8671b7c16da9765e34b5fbb8bedb42819a87da7d
tianjian <1731857742@qq.com>
2018-11-19 15:20:56 +0800

    delete msg

--- spring-cloud-feign-service/src/main/java/com/EurekaServerApplication.java
+++ spring-cloud-feign-service/src/main/java/com/EurekaServerApplication.java
@@ -2,10 +2,13 @@ package com;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableResourceServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(EurekaServerApplication.class)
